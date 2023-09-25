0b9e3f5da243cc8a13566a2a0e79eb02964e8dc3
livia <livianascimento814@gmail.com>
2023-09-25 20:04:59 -0300

    Atividade de array dinâmico

---
+++ src/tipos_abstratos/ArrayDinamico.java
@@ -0,0 +1,94 @@
+package tipos_abstratos;
+
+import java.util.Arrays;
+
+public class ArrayDinamico {
+    private static int[] vetor = new int[10];
+    private static int tamanho = 0;
+
+    public static boolean pesquisarElemento(int elemento) {
+        for (int i = 0; i < tamanho; i++) {
+            if (vetor[i] == elemento) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int menorElemento() {
+        if (tamanho == 0) {
+            throw new IllegalStateException("O vetor está vazio.");
+        }
+
+        int menor = vetor[0];
+        for (int i = 1; i < tamanho; i++) {
+            if (vetor[i] < menor) {
+                menor = vetor[i];
+            }
+        }
+        return menor;
+    }
+
+    public static int maiorElemento() {
+        if (tamanho == 0) {
+            throw new IllegalStateException("O vetor está vazio.");
+        }
+
+        int maior = vetor[0];
+        for (int i = 1; i < tamanho; i++) {
+            if (vetor[i] > maior) {
+                maior = vetor[i];
+            }
+        }
+        return maior;
+    }
+
+    public static int sucessor(int elemento) {
+        for (int i = 0; i < tamanho - 1; i++) {
+            if (vetor[i] == elemento) {
+                return vetor[i + 1];
+            }
+        }
+        throw new IllegalArgumentException("Elemento não encontrado ou já é o último");
+    }
+
+    public static int antecessor(int elemento) {
+        for (int i = 1; i < tamanho; i++) {
+            if (vetor[i] == elemento) {
+                return vetor[i - 1];
+            }
+        }
+        throw new IllegalArgumentException("Elemento não encontrado ou já é o primeiro");
+    }
+
+    public static void inserir(int elemento) {
+        if (tamanho == vetor.length) {
+            vetor = Arrays.copyOf(vetor, vetor.length * 2);
+        }
+        vetor[tamanho] = elemento;
+        tamanho++;
+    }
+
+    public static void deletar(int elemento) {
+        int indice = -1;
+        boolean encontrado = false;
+
+        //buscando o índice do elemento
+        for (int i = 0; i < tamanho; i++) {
+            if (vetor[i] == elemento) {
+                indice = i;
+                encontrado = true;
+            }
+            // se o elemento for encontrado, move os elementos 1 posição
+            if (encontrado && i < tamanho - 1) {
+                vetor[i] = vetor[i + 1];
+            }
+        }
+
+        if (encontrado) {
+            tamanho--;
+        } else {
+            throw new IllegalArgumentException("Elemento não encontrado");
+        }
+    }
+}
